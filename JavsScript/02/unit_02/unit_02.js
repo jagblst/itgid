3ad42568f2ee0c710f335543f324e2a6a2609449
Jaga <jagblst@gmail.com>
2023-01-30 15:05:55 +0200

    SPRINT 02. Основы ввода данных
    homework complete

--- JavsScript/02/unit_02/unit_02.js
+++ JavsScript/02/unit_02/unit_02.js
@@ -53,7 +53,7 @@ document.querySelector('.out-7').textContent = e4 * f4;
 function t8() {
     // value всегда получаем внутри функции
     let data = document.querySelector('.i-8').value; // получили значение
-    document.querySelector('.out-8').textContent = data;// тут выводим
+    document.querySelector('.out-8').textContent = data; // тут выводим
 }
 
 document.querySelector('.b-8').onclick = t8;
@@ -62,9 +62,11 @@ document.querySelector('.b-8').onclick = t8;
 // По нажатию на кнопку .b-9 выполняется функция t9, которая выводит в div.out-9 то, что пользователь ввел в input.i-9. Добавьте очистку input после нажатия кнопки.
 
 function t9() {
-    let data = document.querySelector('.i-9').value;
-    document.querySelector('.out-9').innerHTML = data;
-    data = '';
+    let inputIn = document.querySelector('.i-9');
+    let out = document.querySelector('.out-9');
+    let data = inputIn.value;
+    out.innerHTML = data;
+    inputIn.value = '';
 }
 
 document.querySelector('.b-9').onclick = t9;
@@ -73,7 +75,8 @@ document.querySelector('.b-9').onclick = t9;
 // По нажатию на кнопку .b-10 выполняется функция t10, которая выводит в div.out-10 то, что пользователь ввел в input.i-10 умноженное на 20.
 
 function t10() {
-
+    let data = document.querySelector('.i-10').value;
+    document.querySelector('.out-10').textContent = data * 20;
 }
 
 document.querySelector('.b-10').onclick = t10;
@@ -82,7 +85,8 @@ document.querySelector('.b-10').onclick = t10;
 // По нажатию на кнопку .b-11 выполняется функция t11, которая выводит на страницу в .out-11 число, которое ввел пользователь в i-11 и к нему добавленное число 55. В этой задаче мы не приводим к числу полученное из input.
 
 function t11() {
-
+    let data = document.querySelector('.i-11').value;
+    document.querySelector('.out-11').textContent = data + 55;
 }
 
 document.querySelector('.b-11').onclick = t11;
@@ -91,7 +95,9 @@ document.querySelector('.b-11').onclick = t11;
 // По нажатию на кнопку выполняется функция t12. Функция должна получить данные из input i.12-1 и i.12-2 (пользователь вводин в них фамилию и имя). После чего вывести в .out-12  строку 'Hello имя фамилия', где имя - имя пользователя и фамилия - введенная фамилия. Обращаю ваше внимание - с этого момента мы работаем строго по заданию. Hello - с большой буквы. Между Hello имя один пробел (вы же включили отображение пробелов?). Вы не добавляете "от себя" - запятые, любые другие слова и символы. Аналогичные правила действуют и дальше по курсу.
 
 function t12() {
-
+    let name = document.querySelector('.i-12-1').value;
+    let surName = document.querySelector('.i-12-2').value;
+    document.querySelector('.out-12').innerHTML = `Hello ${name} ${surName}`;
 }
 
 document.querySelector('.b-12').onclick = t12;
@@ -100,16 +106,18 @@ document.querySelector('.b-12').onclick = t12;
 // При нажатии кнопки .b-13 выполняется t13. Функция получает из input.i-13-1 и .i-13-2 числа в переменные a, b. После чего в .out-13 выводит их сумму. Преобразовывайте a, b в числа!!!!
 
 function t13() {
-
+    let a = document.querySelector('.i-13-1').value;
+    let b = document.querySelector('.i-13-2').value;
+    document.querySelector('.out-13').innerHTML = +a + +b;
 }
 
 document.querySelector('.b-13').onclick = t13;
 
 // Task 14.
 // По нажатию кнопки .b-14 выполняется функция t14. Она заменяет значение value в input.i-14 на строку 'Go';
-
+    
 function t14() {
-
+    document.querySelector('.i-14').value = 'Go';
 }
 
 document.querySelector('.b-14').onclick = t14;
@@ -118,7 +126,7 @@ document.querySelector('.b-14').onclick = t14;
 //  По нажатию кнопки .b-15 выполняется функция t15. Она присваивает .i-15 свойство style.border = "4px solid red";
 
 function t15() {
-
+   document.querySelector('.i-15').style.border = "4px solid red";
 }
 
 document.querySelector('.b-15').onclick = t15;
@@ -127,7 +135,9 @@ document.querySelector('.b-15').onclick = t15;
 //По нажатию на кнопку .b-16 выполняется функция t16. Функция получает из .i-16-1 и i-16-2 числа. Выведите в .out-16 сумму данных чисел по нажатию кнопки b-16. Не переводите полученное из input в число!!!!
 
 function t16() {
-
+    let num1 = document.querySelector('.i-16-1').value;
+    let num2 = document.querySelector('.i-16-2').value;
+    document.querySelector('.out-16').innerHTML = num1 + num2;
 }
 
 document.querySelector('.b-16').onclick = t16;
@@ -148,7 +158,8 @@ document.querySelector('.b-17').onclick = t17;
 //  По нажатию на кнопку .b-18 выполняется функция t18. Функция получает из .i-18 число в переменную a. Выводит в out-18 результат операции parseFloat(a). Попробуйте ввести значения 5, 33.02, 33.9a, -20.01
 
 function t18() {
-
+    let a = document.querySelector('.i-18').value;
+    document.querySelector('.out-18').innerHTML = parseFloat(a);
 }
 
 document.querySelector('.b-18').onclick = t18;
@@ -157,7 +168,9 @@ document.querySelector('.b-18').onclick = t18;
 // По нажатию кнопки .b-19 функция t19 дожна в out-19 вывести сумму двух отрицательных чисел из input .i-19-1 и .i-19-2. 
 
 function t19() {
-
+    let num1 = document.querySelector('.i-19-1').value;
+    let num2 = document.querySelector('.i-19-2').value;
+    document.querySelector('.out-19').innerHTML = +num1 + +num2;
 }
 
 document.querySelector('.b-19').onclick = t19;
@@ -169,7 +182,7 @@ let count = 0;
 
 function t20() {
     count = count + 1; // можно сократить count++
-    // тут допишите вывод в out-20
+    document.querySelector('.out-20').innerHTML = count;
 }
 
 document.querySelector('.b-20').onclick = t20;
