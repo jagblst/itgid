9d55180b07d38e2d12c0c5d4b218dce9e8251f99
Jaga <jagblst@gmail.com>
2023-03-10 13:46:21 +0200

    JS SPRINT 28. ООП в ES6
    homework  tasks 1-3 completed

---
+++ JavsScript/28/unit_28/Goods.js
@@ -0,0 +1,28 @@
+class Goods {
+    constructor(name, price, image, count) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.count = count;
+    }
+    draw() {
+        let div = document.createElement('div');
+        div.classList.add('goods');
+
+        let h1 = document.createElement('h1');
+        h1.innerHTML = this.name;
+        div.append(h1);
+
+        let p = document.createElement('p');
+        p.classList.add('price');
+        p.innerHTML = this.price;
+        div.append(p);
+
+        let img = document.createElement('img');
+        img.src = this.image;
+        img.alt = this.name;
+        div.append(img);
+
+        return div;
+    }
+}
